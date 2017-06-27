95ec2379cd1ea0dd6ec6c7b35752e674f8f3359f
Andrzej Dąbrowski <andrzej@ratio.team>
2017-06-27 10:34:20 +0200

    remove unnecessary overflow: hidden from html/body

--- ScrollManager.js
+++ ScrollManager.js
@@ -34,12 +34,32 @@ var ScrollEventsManager = function() {
 
 	var bodyScrollLocked = false;
 
+	var heightBlock = $('<div class="__scroll-events-manager"></div>');
+	heightBlock.css({
+		position: "absolute",
+		top: 0,
+		left: 0,
+		width: 1,
+		"pointer-events": "none"
+	});
+
+
+	var heightBlockOnResize = function() {
+		heightBlock.css('height', window.innerHeight);
+	}
+
+	/* Try with settings scroll to 1 / height - 1, when end of scrolling. This will always focus scroll on the component, never on body behind!!! */
+
 	this.lockScroll = function() {
 		if (bodyScrollLocked) { return; }
 		bodyScrollLocked = true;
 
 		/* Lock body scroll blocks all scroll events registered by register method! */
 		this.block();
+
+		$('body').append(heightBlock);
+		heightBlockOnResize();
+		$(window).resize(heightBlockOnResize);
 
 		var lockScrollPosition = $(window).scrollTop();
 		$('.page-wrapper').addClass('blocked-scroll').scrollTop(lockScrollPosition);
@@ -51,8 +71,10 @@ var ScrollEventsManager = function() {
 
 		var scrollTop = $('.page-wrapper').scrollTop();
 		$('.page-wrapper').removeClass('blocked-scroll');
-
 		$(window).scrollTop(scrollTop);
+
+		$(window).off('resize', heightBlockOnResize);
+		heightBlock.remove();
 
 		var _this = this;
 		setTimeout(function() {
